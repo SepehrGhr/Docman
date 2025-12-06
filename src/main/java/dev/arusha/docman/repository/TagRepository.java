63759ff79396f3f237533d2e4cd82300db0ccbd2
Sepehr <sepehr.ghardashi@gmail.com>
2025-12-06 04:21:21 +0330

    feat: add tag and document repository

---
+++ src/main/java/dev/arusha/docman/repository/TagRepository.java
@@ -0,0 +1,17 @@
+package dev.arusha.docman.repository;
+
+import dev.arusha.docman.model.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Long> {
+
+    List<Tag> findByNameIn(Collection<String> names);
+
+    Optional<Tag> findByName(String name);
+}
